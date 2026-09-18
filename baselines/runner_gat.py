848b6cca420f586154c3557e56bdcc8354bb19b2
Alisa <66892515+DuanhaoranCC@users.noreply.github.com>
2026-09-17 18:45:42 -0700

    Delete baselines directory

--- baselines/runner_gat.py
+++
@@ -1,9 +0,0 @@
-"""GAT baseline dispatch."""
-
-from baselines.runner_supervised_gnn import run_supervised_gnn
-
-
-def run_gat(sources, targets, seeds, device, norm, quick=False, hp=None, progress=None):
-    return run_supervised_gnn(
-        "gat", sources, targets, seeds, device, norm, quick, hp=hp, progress=progress
-    )
--- baselines/runner_gcn.py
+++
@@ -1,9 +0,0 @@
-"""GCN baseline dispatch."""
-
-from baselines.runner_supervised_gnn import run_supervised_gnn
-
-
-def run_gcn(sources, targets, seeds, device, norm, quick=False, hp=None, progress=None):
-    return run_supervised_gnn(
-        "gcn", sources, targets, seeds, device, norm, quick, hp=hp, progress=progress
-    )
